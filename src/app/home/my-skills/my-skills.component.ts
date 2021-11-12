987980081b1c261ae27f8a46bfead66a4df3f94a
Sebastian Ebel <sebastianebel@hotmail.de>
2021-11-12 20:43:48 +0100

    slide in added by my skills

--- src/app/home/my-skills/my-skills.component.ts
+++ src/app/home/my-skills/my-skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MySkillsComponent implements OnInit {
 
-  constructor() { }
+  scrolling: boolean;
+  constructor() {
+      this.scrolling = false;
+    }
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any){
+   // console.log($event['Window']);
+    
+    if(!this.scrolling) {
+      this.scrolling = true;
+    }
+    
+ }
 
   ngOnInit(): void {
   }
-
 }
