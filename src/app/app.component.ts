1bedb9f53b1ee292cb9fab615d127dc908d39290
Sebastian Ebel <sebastianebel@hotmail.de>
2021-11-16 20:23:30 +0100

    slide in optimized changes in codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, state, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
--- src/app/home/my-skills/my-skills.component.ts
+++ src/app/home/my-skills/my-skills.component.ts
@@ -1,27 +1,71 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, HostListener, Inject, Input, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
+  animations:[
+    trigger('myInsertTrigger', [
+      transition(':enter', [
+        style({transform: 'translateX(500px)',
+          opacity: 0}),
+          animate('1s ease-in-out', style({transform: 'translateX(0px)',
+            opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('myTrigger', [
+      transition(':enter', [
+        style({transform: 'translateX(1000px)',
+          opacity: 0}),
+          animate('1.5s 1s ease-in-out', style({transform: 'translateX(0px)',
+            opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('myTrigger1', [
+      transition(':enter', [
+        style({transform: 'translateX(1000px)',
+          opacity: 0}),
+          animate('2s 2s ease-in-out', style({transform: 'translateX(0px)',
+            opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ opacity: 0 }))
+      ])
+    ]),
+  ],
   templateUrl: './my-skills.component.html',
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent implements OnInit {
 
-  public scroll = false;
-  constructor(@Inject(DOCUMENT) private document: any) {
+  isOpenH2 = false;
+  isOpenDiv = false;
+  isOpenDiv1 = false;
+
+  constructor() {
     }
 
   @HostListener('window:scroll', ['$event']) onScrollEvent(event: any){
-   let position = window.scrollY;
+
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop)
     
-    if(position > 100) {
-      this.scroll = true;
+    if(pos){
+      this.isOpenH2 = true;
+      this.isOpenDiv = true;
+      this.isOpenDiv1 = true;
     } else {
-      this.scroll = false;
+      this.isOpenH2 = false;
+      this.isOpenDiv = false;
+      this.isOpenDiv1 = false;
     }
  }
 
   ngOnInit(): void {
+
   }
 }
