f5662339ca4f5a1af21b4e7a60f0b0afa78f1f40
Sebastian Ebel <sebastianebel@hotmail.de>
2021-11-17 17:35:00 +0100

    slide in optimized changes in contact me

--- src/app/home/contact-me/contact-me.component.ts
+++ src/app/home/contact-me/contact-me.component.ts
@@ -1,26 +1,60 @@
-import { DOCUMENT } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
+  animations:[
+    trigger('myH1Trigger', [
+      transition(':enter', [
+        style({transform: 'translateX(500px)',
+          opacity: 0}),
+          animate('1s ease-in-out', style({transform: 'translateX(0px)',
+            opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('mySpanTrigger', [
+      transition(':enter', [
+        style({transform: 'translateX(1000px)',
+          opacity: 0}),
+          animate('1s 1s ease-in-out', style({transform: 'translateX(0px)',
+            opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('myFormTrigger', [
+      transition(':enter', [
+        style({transform: 'translateX(1000px)',
+          opacity: 0}),
+          animate('1s 2s ease-in-out', style({transform: 'translateX(0px)',
+            opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ opacity: 0 }))
+      ])
+    ]),
+  ],
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
 export class ContactMeComponent implements OnInit {
   
-  public scroll = false;
-  constructor(@Inject(DOCUMENT) private document: any) {
+  isOpen = false;
+  constructor() {
     }
 
   @HostListener('window:scroll', ['$event']) onScrollEvent(event: any){
-   event = window.scrollY;
-    
-    if(event > 3700) {
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop);
+    if(pos > 3800){
       setTimeout(() => {
-        this.scroll = true;
+        this.isOpen = true;
       }, 1500);
     } else {
-      this.scroll = false;
+      this.isOpen = false;
     }
  }
   ngOnInit(): void {
